03ca0012a946fb1c925bf866e6eec21c76eec28c
Dziadzlasu <18483718+Dziadzlasu@users.noreply.github.com>
2019-04-05 16:19:12 +0200

    Updated master layout styles and helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def login_helper
+  def login_helper(style)
     if current_user.is_a?(GuestUser)
-      (link_to 'Register', new_user_registration_path) +
+      (link_to 'Register', new_user_registration_path, class: style) +
         '<br>'.html_safe +
-        (link_to 'Login', new_user_session_path)
+        (link_to 'Login', new_user_session_path, class: style)
     else
-      link_to 'Logout', destroy_user_session_path, method: :delete
+      link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
@@ -18,6 +18,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    DziadDevcampViewTool::Renderer.copyright 'Dziad', 'All rights reserved'
+    DziadDevcampViewTool::Renderer.copyright 'Dariusz Kłonowski', 'All rights reserved'
   end
 end
